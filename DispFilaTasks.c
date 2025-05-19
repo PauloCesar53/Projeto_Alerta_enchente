2a233744d1e49e463474455ca30f71e0413dfc0f
PauloCesar53 <pcdilauro53@gmail.com>
2025-05-19 15:38:37 -0300

    add funcionalidade matriz de LEDs

--- DispFilaTasks.c
+++ DispFilaTasks.c
@@ -23,6 +23,22 @@
 #include "queue.h"
 #include <stdio.h>
 #define buzzer 21// pino do Buzzer na BitDogLab
+//matriz led
+#include "hardware/pio.h"
+#include "ws2812.pio.h"
+#define IS_RGBW false
+#define NUM_PIXELS 25
+#define WS2812_PIN 7
+// Variável global para armazenar a cor (Entre 0 e 255 para intensidade)
+uint8_t led_r = 0;  // Intensidade do vermelho
+uint8_t led_g = 0; // Intensidade do verde
+uint8_t led_b = 0; // Intensidade do azul 
+bool led_buffer[NUM_PIXELS];// Variável (protótipo)
+
+//protótipo funções que ligam leds da matriz 5x5
+static inline void put_pixel(uint32_t pixel_grb);
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b);
+void set_one_led(uint8_t r, uint8_t g, uint8_t b);//liga os LEDs escolhidos 
 
 
 #define I2C_PORT i2c1
@@ -202,6 +218,38 @@ void vLedTask(void *params)//Liga lEDs a depender do tipo de alerta
         vTaskDelay(pdMS_TO_TICKS(50)); // Atualiza a cada 50ms
     }
 }
+void vMatrizTask()//task alerta na  Matriz
+{
+    //configuração PIO
+    PIO pio = pio0;
+    int sm = 0;
+    uint offset = pio_add_program(pio, &ws2812_program);
+    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
+    joystick_data_t joydata;
+    while (true)
+    {   
+        if(xQueueReceive(xQueueJoystickData, &joydata, portMAX_DELAY) == pdTRUE){//modo diurno
+            uint joy_x=joydata.x_pos/4095.0*100;
+            uint joy_y=joydata.y_pos/4095.0*100;
+            
+            if(joy_x>=70 && joy_y<=80){//alerta de nivél rio apenas (azul acende)
+                set_one_led(led_r, led_g, 5);//liga azul com intensidade 5
+                vTaskDelay(pdMS_TO_TICKS(1500));
+                set_one_led(0, 0, 0);//apaga matriz
+                vTaskDelay(pdMS_TO_TICKS(500));
+            }else if(joy_y>=80 && joy_x<=70){//apenas Volume chuva alto (Liga LED verde)
+                set_one_led(led_r, 5, led_b);// liga verde com intensidade 5
+            }else if(joy_x>=80 && joy_y>=70){//volume chuva e rios altos simultaneamente (Liga LED vermelho)
+                set_one_led(5, led_g, led_b);// liga vermelho com intensidade 5
+                vTaskDelay(pdMS_TO_TICKS(500));
+                set_one_led(led_r, led_g, led_b);
+                vTaskDelay(pdMS_TO_TICKS(500));
+            }else{
+                set_one_led(led_r, led_g, led_b);// desliga matriz
+            }
+        }
+    }
+}
 
 
 // Modo BOOTSEL com botão B
@@ -229,8 +277,41 @@ int main()
     xTaskCreate(vJoystickTask, "Joystick Task", 256, NULL, 1, NULL);
     xTaskCreate(vDisplayTask, "Display Task", 512, NULL, 1, NULL);
     xTaskCreate(vBuzzerTask, "Buzzer Task", 256, NULL, 1, NULL);
+    xTaskCreate(vMatrizTask, "Matriz Task", 256, NULL, 1, NULL);
     xTaskCreate(vLedTask, "LED RGB Task", 256, NULL, 1, NULL);
     // Inicia o agendador
     vTaskStartScheduler();
     panic_unsupported();
+}
+bool led_buffer[NUM_PIXELS] = { //Buffer para armazenar quais LEDs estão ligados matriz 5x5
+    0, 0, 0, 0, 0,
+    0, 1, 1, 1, 0,
+    0, 1, 1, 1, 0,
+    0, 1, 1, 1, 0,
+    0, 0, 0, 0, 0};
+static inline void put_pixel(uint32_t pixel_grb)
+{
+    pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
+}
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b)
+{
+    return ((uint32_t)(r) << 8) | ((uint32_t)(g) << 16) | (uint32_t)(b);
+}
+void set_one_led(uint8_t r, uint8_t g, uint8_t b)
+{
+    // Define a cor com base nos parâmetros fornecidos
+    uint32_t color = urgb_u32(r, g, b);
+
+    // Define todos os LEDs com a cor especificada
+    for (int i = 0; i < NUM_PIXELS; i++)
+    {
+        if (led_buffer[i])
+        {
+            put_pixel(color); // Liga o LED com um no buffer
+        }
+        else
+        {
+            put_pixel(0); // Desliga os LEDs com zero no buffer
+        }
+    }
 }
